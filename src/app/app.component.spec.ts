7361a56ed2b715b09a50dda213ceddb89d1d3d10
Meio <jeromeio.velarde@gmail.com>
2021-10-10 21:51:53 +0800

    Initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stack-iamge-app'`, () => {
+  it(`should have as title 'stack-image-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('stack-iamge-app');
+    expect(app.title).toEqual('stack-image-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('stack-iamge-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('stack-image-app app is running!');
   });
 });
